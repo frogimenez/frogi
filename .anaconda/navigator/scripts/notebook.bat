156fb9e4d9736756e1ec17e3b9bb3204324d9dc0
Fernando Rodriguez <frogimenez@gmail.com>
2020-10-16 17:07:29 +0200

    first commit angular course

---
+++ .anaconda/navigator/scripts/notebook.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\frogi\Anaconda3\Scripts\activate C:\Users\frogi\Anaconda3
+jupyter-notebook >C:\Users\frogi\.anaconda\navigator\scripts\notebook-out-2.txt 2>C:\Users\frogi\.anaconda\navigator\scripts\notebook-err-2.txt
